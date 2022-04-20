df5bab9ec3339700611bee8d96c83d3e56f85c88
ruben.aguilera <ruben.aguilera@kairosds.com>
2022-04-20 16:34:10 +0200

    chore: initial commit

---
+++ src/test/java/com/acme/zipi/ZipiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.zipi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
