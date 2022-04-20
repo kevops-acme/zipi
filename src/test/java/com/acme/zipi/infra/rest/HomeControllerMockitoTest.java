1f1b021a300e82dfd26ef454b8af9d3b11a3a159
ruben.aguilera <ruben.aguilera@kairosds.com>
2022-04-20 23:10:53 +0200

    test: add home controller units tests

---
+++ src/test/java/com/acme/zipi/infra/rest/HomeControllerMockitoTest.java
@@ -0,0 +1,28 @@
+package com.acme.zipi.infra.rest;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(MockitoExtension.class)
+public class HomeControllerMockitoTest {
+
+    @InjectMocks
+    HomeController homeController;
+
+    @Test
+    public void testHomeController() {
+        ResponseEntity<String> responseEntity = homeController.getUsers();
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals("Hello World!", responseEntity.getBody());
+    }
+    
+
+
+}
---
+++ src/test/java/com/acme/zipi/infra/rest/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.acme.zipi.infra.rest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@WebMvcTest(controllers = HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomeController() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/")).andExpect(status().isOk()).andReturn();
+
+        assertEquals("Hello World!", mvcResult.getResponse().getContentAsString());
+    }
+
+}
